3fec7de79049d14c18f1f61d7570fe1e06c47fb5
dead_horse <dead_horse@qq.com>
2016-02-03 15:26:16 +0800

    feat: support name@spec

--- bin/install.js
+++ bin/install.js
@@ -16,11 +16,14 @@
 
 const co = require('co');
 const npminstall = require('../');
-
+const npa = require('npm-package-arg');
 const names = process.argv.slice(2);
 const pkgs = [];
+
+//
 for (const name of names) {
-  pkgs.push({ name });
+  const p = npa(name);
+  pkgs.push({ name: p.name, version: p.spec });
 }
 
 co(function*() {
