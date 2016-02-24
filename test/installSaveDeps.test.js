1ebb8c90b404a5e1d535d98224de4d557f9c8131
fengmk2 <m@fengmk2.com>
2016-02-25 00:51:26 +0800

    test: add npm_registry env for ci

--- test/installSaveDeps.test.js
+++ test/installSaveDeps.test.js
@@ -20,78 +20,80 @@ const fs = require('mz/fs');
 const coffee = require('coffee');
 const npminstall = path.join(__dirname, '..', 'bin', 'install.js');
 
-describe('test/installSaveDeps.test.js', () => {
-  const tmp = path.join(__dirname, 'fixtures', 'tmp');
+if (process.platform !== 'win32') {
+  describe('test/installSaveDeps.test.js', () => {
+    const tmp = path.join(__dirname, 'fixtures', 'tmp');
 
-  beforeEach(() => {
-    rimraf.sync(tmp);
-    mkdirp.sync(tmp);
-  });
-  afterEach(() => {
-    rimraf.sync(tmp);
-  });
+    beforeEach(() => {
+      rimraf.sync(tmp);
+      mkdirp.sync(tmp);
+    });
+    afterEach(() => {
+      rimraf.sync(tmp);
+    });
 
-  it('should install --save pedding and update dependencies', done => {
-    coffee.fork(npminstall, [
-      '--save',
-      'pedding',
-    ], {
-      cwd: tmp,
-    })
-    .expect('stdout', /\[pedding@\*\] installed/)
-    .expect('code', 0)
-    .end(err => {
-      assert(!err, err && err.message);
+    it('should install --save pedding and update dependencies', done => {
+      coffee.fork(npminstall, [
+        '--save',
+        'pedding',
+      ], {
+        cwd: tmp,
+      })
+      .expect('stdout', /\[pedding@\*\] installed/)
+      .expect('code', 0)
+      .end(err => {
+        assert(!err, err && err.message);
 
-      const deps = JSON.parse(fs.readFileSync(path.join(tmp, 'package.json'))).dependencies;
-      assert(deps);
-      assert(deps.pedding);
-      assert.equal(typeof deps.pedding, 'string');
-      assert(/^[\^\~]{1}\d+\.\d+\.\d+/.test(deps.pedding), deps.pedding);
+        const deps = JSON.parse(fs.readFileSync(path.join(tmp, 'package.json'))).dependencies;
+        assert(deps);
+        assert(deps.pedding);
+        assert.equal(typeof deps.pedding, 'string');
+        assert(/^[\^\~]{1}\d+\.\d+\.\d+/.test(deps.pedding), deps.pedding);
 
-      done();
+        done();
+      });
+    });
+
+    it('should install --save-dev pedding and update devDependencies', done => {
+      coffee.fork(npminstall, [
+        '--save-dev',
+        'pedding@0',
+      ], {
+        cwd: tmp,
+      })
+      .expect('stdout', /\[pedding@0\] installed/)
+      .expect('code', 0)
+      .end(err => {
+        assert(!err, err && err.message);
+        const deps = JSON.parse(fs.readFileSync(path.join(tmp, 'package.json'))).devDependencies;
+        assert(deps);
+        assert(deps.pedding);
+        assert.equal(typeof deps.pedding, 'string');
+        assert(/^[\^\~]{1}\d+\.\d+\.\d+/.test(deps.pedding), deps.pedding);
+
+        done();
+      });
+    });
+
+    it('should install --save-optional pedding and update optionalDependencies', done => {
+      coffee.fork(npminstall, [
+        '--save-optional',
+        'pedding@1',
+      ], {
+        cwd: tmp,
+      })
+      .expect('stdout', /\[pedding@1\] installed/)
+      .expect('code', 0)
+      .end(err => {
+        assert(!err, err && err.message);
+        const deps = JSON.parse(fs.readFileSync(path.join(tmp, 'package.json'))).optionalDependencies;
+        assert(deps);
+        assert(deps.pedding);
+        assert.equal(typeof deps.pedding, 'string');
+        assert(/^[\^\~]{1}\d+\.\d+\.\d+/.test(deps.pedding), deps.pedding);
+
+        done();
+      });
     });
   });
-
-  it('should install --save-dev pedding and update devDependencies', done => {
-    coffee.fork(npminstall, [
-      '--save-dev',
-      'pedding@0',
-    ], {
-      cwd: tmp,
-    })
-    .expect('stdout', /\[pedding@0\] installed/)
-    .expect('code', 0)
-    .end(err => {
-      assert(!err, err && err.message);
-      const deps = JSON.parse(fs.readFileSync(path.join(tmp, 'package.json'))).devDependencies;
-      assert(deps);
-      assert(deps.pedding);
-      assert.equal(typeof deps.pedding, 'string');
-      assert(/^[\^\~]{1}\d+\.\d+\.\d+/.test(deps.pedding), deps.pedding);
-
-      done();
-    });
-  });
-
-  it('should install --save-optional pedding and update optionalDependencies', done => {
-    coffee.fork(npminstall, [
-      '--save-optional',
-      'pedding@1',
-    ], {
-      cwd: tmp,
-    })
-    .expect('stdout', /\[pedding@1\] installed/)
-    .expect('code', 0)
-    .end(err => {
-      assert(!err, err && err.message);
-      const deps = JSON.parse(fs.readFileSync(path.join(tmp, 'package.json'))).optionalDependencies;
-      assert(deps);
-      assert(deps.pedding);
-      assert.equal(typeof deps.pedding, 'string');
-      assert(/^[\^\~]{1}\d+\.\d+\.\d+/.test(deps.pedding), deps.pedding);
-
-      done();
-    });
-  });
-});
+}
