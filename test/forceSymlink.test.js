04f4ca19735c1471c514679372e1ba9a786adcda
dead_horse <dead_horse@qq.com>
2016-02-26 15:24:39 +0800

    fix: cleanup before link, fixes #22

---
+++ test/forceSymlink.test.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright(c) cnpm and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *   dead_horse <dead_horse@qq.com>
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const assert = require('assert');
+const path = require('path');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+const readJSON = require('../lib/utils').readJSON;
+const npminstall = require('../');
+
+describe('test/forceSymlink.test.js', function() {
+  const tmp = path.join(__dirname, 'fixtures', 'tmp');
+
+  function cleanup() {
+    rimraf.sync(tmp);
+  }
+
+  beforeEach(() => {
+    cleanup();
+    mkdirp.sync(tmp);
+  });
+  afterEach(cleanup);
+
+  it('should remove exist module first', function*() {
+    mkdirp.sync(path.join(tmp, 'node_modules/debug'));
+    yield npminstall({
+      root: tmp,
+      pkgs: [
+        { name: 'debug', version: '1.0.0' },
+      ],
+    });
+    const pkg = yield readJSON(path.join(tmp, 'node_modules/debug/package.json'));
+    assert.equal(pkg.name, 'debug');
+    assert.equal(pkg.version, '1.0.0');
+  });
+});
