8676cd0e2245bee53826d89dc4e79fbac1a0530b
fengmk2 <m@fengmk2.com>
2016-05-04 21:08:05 +0800

    fix: add retry to get binary-mirror-config/latest (#77)
    make sure info exists

--- bin/install.js
+++ bin/install.js
@@ -145,17 +145,7 @@ co(function*() {
   let binaryMirrors = {};
 
   if (inChina) {
-    const binaryMirrorUrl = registry + '/binary-mirror-config/latest';
-    try {
-      const res = yield get(binaryMirrorUrl, {
-        dataType: 'json',
-        followRedirect: true,
-      });
-      binaryMirrors = res.data.mirrors.china;
-    } catch (err) {
-      console.warn('Get %s error: %s', binaryMirrorUrl, err.stack);
-      binaryMirrors = require('binary-mirror-config/package.json').mirrors.china;
-    }
+    binaryMirrors = yield getBinaryMirrors(registry);
   }
 
   const config = {
@@ -262,4 +252,35 @@ function* updateDependencies(root, pkgs, propName, saveExact) {
   }
   pkg[propName] = newDeps;
   yield fs.writeFile(pkgFile, JSON.stringify(pkg, null, 2));
+}
+
+function* getBinaryMirrors(registry) {
+  const registries = [ registry ].concat([
+    'https://registry.npm.taobao.org',
+    'https://r.cnpmjs.org',
+    'https://registry.npmjs.org',
+  ]);
+  let lastErr;
+  let binaryMirrors;
+  for (const registry of registries) {
+    const binaryMirrorUrl = registry + '/binary-mirror-config/latest';
+    try {
+      const res = yield get(binaryMirrorUrl, {
+        dataType: 'json',
+        followRedirect: true,
+      });
+      binaryMirrors = res.data.mirrors.china;
+      debug('GET %s => %j', binaryMirrorUrl, binaryMirrors);
+      break;
+    } catch (err) {
+      lastErr = err;
+    }
+  }
+
+  if (!binaryMirrors) {
+    console.warn('Get /binary-mirror-config/latest from %s error: %s', registry, lastErr.stack);
+    binaryMirrors = require('binary-mirror-config/package.json').mirrors.china;
+  }
+
+  return binaryMirrors;
 }
