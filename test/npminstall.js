dd4a84051fbe63e369b976cd039abd51823b451d
fengmk2 <m@fengmk2.com>
2016-05-08 01:01:50 +0800

    feat: support package.json#os property
    closes #79

--- test/npminstall.js
+++ test/npminstall.js
@@ -14,23 +14,25 @@
 
 const npminstall = require('../');
 const config = require('../lib/config');
+const utils = require('../lib/utils');
 
 module.exports = function*(options) {
-  formatOptions(options);
+  yield formatOptions(options);
   return yield npminstall(options);
 };
 
 module.exports.installGlobal = function*(options) {
-  formatOptions(options);
+  yield formatOptions(options);
   return yield npminstall.installGlobal(options);
 };
 
-function formatOptions(options) {
+function* formatOptions(options) {
   if (process.env.local) {
     options.registry = config.chineseRegistry;
     options.env = options.env || {};
     for (const key in config.chineseMirrorEnv) {
       options.env[key] = config.chineseMirrorEnv[key];
     }
+    options.binaryMirrors = yield utils.getBinaryMirrors(options.registry);
   }
 }
