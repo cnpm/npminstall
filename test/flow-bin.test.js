9b33acc4e256ee5fed209a77bc86be5b808515a1
fengmk2 <m@fengmk2.com>
2016-06-26 16:53:52 +0800

    feat: support flow-bin mirror (#87)
    closes https://github.com/cnpm/cnpm/issues/117

---
+++ test/flow-bin.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const path = require('path');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+const npminstall = require('./npminstall');
+const utils = require('../lib/utils');
+
+describe('test/flow-bin.test.js', () => {
+  const tmp = path.join(__dirname, 'fixtures', 'tmp');
+
+  function cleanup() {
+    rimraf.sync(tmp);
+  }
+
+  beforeEach(() => {
+    cleanup();
+    mkdirp.sync(tmp);
+  });
+  afterEach(cleanup);
+
+  it('should install flow-bin from china mirror', function*() {
+    const registry = process.env.local ? 'https://registry.npm.taobao.org' : 'https://registry.npmjs.com';
+    const binaryMirrors = yield utils.getBinaryMirrors(registry);
+    yield npminstall({
+      root: tmp,
+      pkgs: [
+        { name: 'flow-bin' },
+      ],
+      binaryMirrors,
+    });
+  });
+});
