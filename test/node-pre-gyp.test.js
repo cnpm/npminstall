5bac484b5bdece520039ff315d97390956e86969
fengmk2 <m@fengmk2.com>
2016-09-19 15:53:33 +0800

    fix: binary mirror could be http protocol (#128)
    ignore node-pre-gyp http protocol checker

---
+++ test/node-pre-gyp.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const path = require('path');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+const npminstall = require('./npminstall');
+
+describe('test/node-pre-gyp.test.js', () => {
+  const tmp = path.join(__dirname, 'fixtures', 'tmp');
+
+  function cleanup() {
+    rimraf.sync(tmp);
+  }
+
+  beforeEach(() => {
+    cleanup();
+    mkdirp.sync(tmp);
+  });
+  afterEach(cleanup);
+
+  it('should download from http mirror work fine', function* () {
+    yield npminstall({
+      root: tmp,
+      pkgs: [
+        { name: 'sqlite3', version: '3' },
+      ],
+      production: true,
+      cacheDir: '',
+      customBinaryMirrors: {
+        sqlite3: {
+          host: 'http://cdn.npm.taobao.org/dist',
+        },
+      },
+    });
+  });
+});
