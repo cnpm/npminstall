0b90391872d4b0214321601ff8cf0ffc38cc065d
fengmk2 <m@fengmk2.com>
2016-10-12 11:19:46 +0800

    feat: support npm update feat (#133)
    * feat: support npm update feat
    
    just rm node_module and run npminstall again
    
    closes https://github.com/cnpm/npminstall/issues/132
    
    * test: add ant-design-mobile for install case
    
    * test: add fixtures/update for update.test.js

--- test/git-clone-install.sh
+++ test/git-clone-install.sh
@@ -3,6 +3,13 @@
 git clone https://github.com/ant-design/ant-design.git
 cd ant-design
 node ../bin/install.js
+# npmupdate
+node ../bin/update.js
+cd ..
+
+git clone https://github.com/ant-design/ant-design-mobile.git
+cd ant-design-mobile
+node ../bin/install.js
 cd ..
 
 git clone https://github.com/mapbox/node-sqlite3.git
---
+++ test/update.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const assert = require('power-assert');
+const path = require('path');
+const fs = require('fs');
+const rimraf = require('rimraf');
+const coffee = require('coffee');
+const npminstall = path.join(__dirname, '../bin/install.js');
+const npmupdate = path.join(__dirname, '../bin/update.js');
+
+describe('test/update.test.js', () => {
+  const root = path.join(__dirname, 'fixtures', 'update');
+
+  function cleanup() {
+    rimraf.sync(path.join(root, 'node_modules'));
+  }
+
+  beforeEach(done => {
+    cleanup();
+    coffee.fork(npminstall, [], {
+      cwd: root,
+      stdio: 'pipe',
+    })
+    .debug()
+    .end(done);
+  });
+
+  afterEach(cleanup);
+
+  it('should uninstall ok', done => {
+    coffee.fork(npmupdate, [], {
+      cwd: root,
+      stdio: 'pipe',
+    })
+    .debug()
+    .end(err => {
+      assert(!err);
+      assert(fs.existsSync(path.join(root, 'node_modules/pedding')));
+      assert(fs.existsSync(path.join(root, 'node_modules/pkg')));
+      done();
+    });
+  });
+});
