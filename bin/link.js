7b32c77dd844319fed3e1941418d923ed7320ad1
fengmk2 <m@fengmk2.com>
2016-11-22 01:38:53 +0800

    feat: support `$ npmlink <folder>` (#169)

---
+++ bin/link.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const co = require('co');
+const assert = require('assert');
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('mz/fs');
+const parseArgs = require('minimist');
+const utils = require('../lib/utils');
+const bin = require('../lib/bin');
+
+const orignalArgv = process.argv.slice(2);
+const argv = parseArgs(orignalArgv, {
+  string: [
+    'root',
+  ],
+  boolean: [
+    'version',
+    'help',
+  ],
+});
+
+if (argv.version) {
+  console.log('v%s', require('../package.json').version);
+  process.exit(0);
+}
+
+if (argv.help) {
+  console.log(
+`
+Usage:
+
+  npmlink <folder>
+
+Can specify one or more: npmlink /some/folder1 /some/folder2
+`
+  );
+  process.exit(0);
+}
+
+const folders = [];
+const root = argv.root || process.cwd();
+
+for (const name of argv._) {
+  let folder = utils.formatPath(name);
+  if (!path.isAbsolute(folder)) {
+    folder = path.join(root, folder);
+  }
+  assert(fs.existsSync(folder), `${folder} not exists`);
+  folders.push(folder);
+}
+
+co(function* npmlink() {
+  const targetDir = path.join(root, 'node_modules');
+  for (const folder of folders) {
+    // 1. cd folder && npminstall
+    // 2. link folder to CWD/node_modules/{name}
+    // 3. link bins
+    const pkgFile = path.join(folder, 'package.json');
+    const pkg = yield utils.readJSON(pkgFile);
+    assert(pkg.name, `package.name not eixsts on ${pkgFile}`);
+    const linkDir = path.join(targetDir, pkg.name);
+
+    console.info(chalk.gray(`Linking ${folder} to ${linkDir}`));
+    const installArgs = [];
+    for (const arg of orignalArgv) {
+      if (arg.startsWith('--root')) {
+        continue;
+      }
+      if (arg[0] !== '-') {
+        continue;
+      }
+      installArgs.push(arg);
+    }
+    console.info(chalk.gray(`\`$ npminstall ${installArgs.join(' ')}\` on ${folder}`));
+    const installBin = path.join(__dirname, 'install.js');
+    yield utils.fork(installBin, installArgs, {
+      cwd: folder,
+    });
+    yield utils.forceSymlink(folder, linkDir);
+    yield bin(root, pkg, linkDir, { console });
+  }
+}).catch(err => {
+  console.error(chalk.red(err.stack));
+  console.error(chalk.yellow('npmlink version: %s'), require('../package.json').version);
+  console.error(chalk.yellow('npmlink args: %s'), process.argv.join(' '));
+  process.exit(1);
+});
---
+++ test/link-folder.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const assert = require('power-assert');
+const path = require('path');
+const rimraf = require('rimraf');
+const coffee = require('coffee');
+const fs = require('mz/fs');
+const npmlink = path.join(__dirname, '../bin/link.js');
+
+describe('test/link-folder.test.js', () => {
+  const root = path.join(__dirname, 'fixtures', 'link-demo');
+
+  function cleanup() {
+    rimraf.sync(path.join(root, 'node_modules'));
+    rimraf.sync(path.join(root, 'linked-package/node_modules'));
+    rimraf.sync(path.join(root, 'linked-package-2/node_modules'));
+  }
+
+  beforeEach(cleanup);
+  afterEach(cleanup);
+
+  it('should link one folder work', function* () {
+    yield coffee.fork(npmlink, [ 'linked-package' ], {
+      cwd: root,
+    })
+    .debug()
+    .end();
+
+    assert(fs.existsSync(path.join(root, 'node_modules/linked-package')));
+  });
+
+  it('should link two folder work', function* () {
+    yield coffee.fork(npmlink, [ 'linked-package', 'linked-package-2', '-d' ], {
+      cwd: root,
+    })
+    .debug()
+    .end();
+
+    assert(fs.existsSync(path.join(root, 'node_modules/linked-package')));
+    assert(fs.existsSync(path.join(root, 'node_modules/linked-package-2')));
+    // bin file should be exists
+    assert(fs.existsSync(path.join(root, 'node_modules/.bin/linked-package-2')));
+  });
+});
