bc151ef637de5a61e01ab850b2f0fd269c325e46
fengmk2 <fengmk2@gmail.com>
2017-02-03 19:01:25 +0800

    fix: get max satisfy version from root dependencies instead of *
    closes https://github.com/cnpm/npminstall/issues/192

---
+++ test/use-exists-version.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const coffee = require('coffee');
+const rimraf = require('rimraf');
+const path = require('path');
+const assert = require('assert');
+
+describe('use-exists-version.test.js', () => {
+  const tmp = path.join(__dirname, 'fixtures', 'try-to-use-one-version');
+  const bin = path.join(__dirname, '../bin/install.js');
+
+  function cleanup() {
+    rimraf.sync(path.join(tmp, 'node_modules'));
+  }
+
+  beforeEach(cleanup);
+
+  it('should replace tarball url to other', function* () {
+    yield coffee.fork(bin, [ '-d' ], { cwd: tmp })
+      .debug()
+      .expect('code', 0)
+      .expect('stdout', /All packages installed/)
+      .end();
+    const pkg = require(path.join(tmp, 'node_modules/@types/react-dom/node_modules/@types/react/package.json'));
+    assert(pkg.version === '15.0.4');
+  });
+});
