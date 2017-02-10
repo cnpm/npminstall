f6c680a810b302f24a9656acb070abd952b4d33d
Yiyu He <dead-horse@users.noreply.github.com>
2017-02-10 12:29:18 +0800

    * fix: use ancestor's dependence even version not change (#198)
    * test: add more asserts
    * chore: add debug

--- test/flatten.test.js
+++ test/flatten.test.js
@@ -54,4 +54,26 @@ describe('use-exists-version.test.js', () => {
     assert(getPkg('node_modules/mod1/node_modules/mod3/node_modules/koa/package.json').version !== '1.1.0');
     assert(getPkg('node_modules/mod1/node_modules/mod4/node_modules/koa/package.json').version === '0.10.0');
   });
+
+  it('should use first debug@2.2.0', function* () {
+    yield coffee.fork(bin, [ '-d', '--flatten', './mod5' ], { cwd: tmp })
+      .debug()
+      .expect('code', 0)
+      .expect('stdout', /All packages installed/)
+      .end();
+    assert(getPkg('node_modules/mod5/node_modules/mod6/node_modules/mod7/node_modules/debug/package.json').version === '2.2.0');
+    assert(getPkg('node_modules/mod5/node_modules/mod6/node_modules/debug/package.json').version === '0.7.4');
+    assert(getPkg('node_modules/mod5/node_modules/debug/package.json').version === '2.2.0');
+  });
+
+  it('should not use first debug@2.2.0 without flatten flag', function* () {
+    yield coffee.fork(bin, [ '-d', './mod5' ], { cwd: tmp })
+      .debug()
+      .expect('code', 0)
+      .expect('stdout', /All packages installed/)
+      .end();
+    assert(getPkg('node_modules/mod5/node_modules/mod6/node_modules/mod7/node_modules/debug/package.json').version !== '2.2.0');
+    assert(getPkg('node_modules/mod5/node_modules/mod6/node_modules/debug/package.json').version === '0.7.4');
+    assert(getPkg('node_modules/mod5/node_modules/debug/package.json').version === '2.2.0');
+  });
 });
