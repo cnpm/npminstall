8352b661e86268fcebb48b6b5bde71ddb8539eec
dead-horse <dead_horse@qq.com>
2017-02-28 15:38:38 +0800

    fix: ensure peer dependencies flatten

--- test/peer.test.js
+++ test/peer.test.js
@@ -7,38 +7,61 @@ const assert = require('assert');
 const fs = require('fs');
 
 describe('peer.test.js', () => {
-  const tmp = path.join(__dirname, 'fixtures', 'peer');
   const bin = path.join(__dirname, '../bin/install.js');
 
-  function getPkg(subPath) {
-    return JSON.parse(fs.readFileSync(path.join(tmp, subPath)));
-  }
+  describe('unmet root and link', () => {
+    const tmp = path.join(__dirname, 'fixtures', 'peer');
+    function cleanup() {
+      rimraf.sync(path.join(tmp, 'node_modules'));
+    }
 
-  function cleanup() {
-    rimraf.sync(path.join(tmp, 'node_modules'));
-  }
+    beforeEach(cleanup);
+    afterEach(cleanup);
 
-  beforeEach(cleanup);
-  afterEach(cleanup);
+    function getPkg(subPath) {
+      return JSON.parse(fs.readFileSync(path.join(tmp, subPath)));
+    }
 
-  it('should use ancestor\'s dependency for peerDependencies', function* () {
-    yield coffee.fork(bin, [], { cwd: tmp })
-      .debug()
-      .expect('code', 0)
-      .end();
-    assert(getPkg('node_modules/antd-tools/node_modules/tslint/node_modules/typescript/package.json').version === '2.1.6');
-    assert(getPkg('node_modules/antd-tools/node_modules/gulp-typescript/node_modules/typescript/package.json').version === '2.1.6');
-    assert(getPkg('node_modules/antd-tools/node_modules/typescript/package.json').version === '2.1.6');
+    it('should use ancestor\'s dependency for peerDependencies', function* () {
+      yield coffee.fork(bin, [], { cwd: tmp })
+        .debug()
+        .expect('code', 0)
+        .end();
+      assert(getPkg('node_modules/antd-tools/node_modules/tslint/node_modules/typescript/package.json').version === '2.1.6');
+      assert(getPkg('node_modules/antd-tools/node_modules/gulp-typescript/node_modules/typescript/package.json').version === '2.1.6');
+      assert(getPkg('node_modules/antd-tools/node_modules/typescript/package.json').version === '2.1.6');
+    });
+
+    it('should ignore peerDependency if in dependencies', function* () {
+      yield coffee.fork(bin, [ 'react-countup@1.3.0' ], { cwd: tmp })
+        .debug()
+        .expect('code', 0)
+        .end();
+      assert(getPkg('node_modules/react-countup/node_modules/react/package.json').version.startsWith('15.'));
+      const pkg = getPkg('node_modules/react-countup/package.json');
+      assert(pkg.dependencies.react === '^15.3.2');
+      assert(pkg.peerDependencies.react === '>=0.14.0');
+    });
   });
 
-  it('should ignore peerDependency if in dependencies', function* () {
-    yield coffee.fork(bin, [ 'react-countup@1.3.0' ], { cwd: tmp })
-      .debug()
-      .expect('code', 0)
-      .end();
-    assert(getPkg('node_modules/react-countup/node_modules/react/package.json').version.startsWith('15.'));
-    const pkg = getPkg('node_modules/react-countup/package.json');
-    assert(pkg.dependencies.react === '^15.3.2');
-    assert(pkg.peerDependencies.react === '>=0.14.0');
+  describe('match root', () => {
+    const tmp = path.join(__dirname, 'fixtures', 'react-and-react-dom');
+
+    function cleanup() {
+      rimraf.sync(path.join(tmp, 'node_modules'));
+    }
+
+    beforeEach(cleanup);
+    afterEach(cleanup);
+
+    it('should ignore peerDependency match with root', function* () {
+      yield coffee.fork(bin, [], { cwd: tmp })
+        .debug()
+        .expect('code', 0)
+        .end();
+      assert(!fs.existsSync(path.join(tmp, 'node_modules/react-dom/node_modules/react')));
+      assert(fs.existsSync(path.join(tmp, 'node_modules/react-dom')));
+      assert(fs.existsSync(path.join(tmp, 'node_modules/react')));
+    });
   });
 });
