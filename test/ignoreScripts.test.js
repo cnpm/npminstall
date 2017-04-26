ff7af1c4baf8b434c4881ddfd521cd6c03a61c62
dead-horse <dead_horse@qq.com>
2017-04-26 22:54:28 +0800

    fix: remove starting dot to ensure webpack2 work

--- test/ignoreScripts.test.js
+++ test/ignoreScripts.test.js
@@ -23,7 +23,7 @@ describe('test/ignoreScripts.test.js', () => {
     });
 
     const dirs = yield fs.readdir(path.join(root, 'node_modules'));
-    assert.deepEqual(dirs, [ '._pkg@1.0.0@pkg', '.npminstall.done', '.package_versions.json', 'pkg' ]);
+    assert.deepEqual(dirs.sort(), [ '_pkg@1.0.0@pkg', '.npminstall.done', '.package_versions.json', 'pkg' ].sort());
     const files = yield fs.readdir(path.join(root, 'node_modules/pkg'));
     assert.deepEqual(files, [ '.npminstall.done', 'index.js', 'package.json' ]);
   });
