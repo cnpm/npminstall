cf1a3eef2ba6b2cef51888005e7e60935fa3f439
dead-horse <dead_horse@qq.com>
2017-04-27 10:57:00 +0800

    test: add next test case

---
+++ test/next.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+const npminstall = require('./npminstall');
+const exec = require('mz/child_process').exec;
+
+// make sure https://github.com/cnpm/cnpm/issues/194 work!
+describe('test/next.test.js', () => {
+  const tmp = path.join(__dirname, '.tmp', 'next');
+
+  function cleanup() {
+    rimraf.sync(tmp);
+  }
+
+  beforeEach(() => {
+    cleanup();
+    mkdirp.sync(tmp);
+  });
+  afterEach(cleanup);
+
+  it('should next build success', function* () {
+    yield exec(`git clone git@github.com:now-examples/next-news.git ${tmp}`);
+    yield npminstall({
+      root: tmp,
+    });
+    yield exec('npm run build', {
+      cwd: tmp,
+    });
+    assert(fs.existsSync(path.join(tmp, '.next')));
+  });
+});
