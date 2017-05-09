7e54e0d70f9854675add888065247e850043360e
fengmk2 <fengmk2@gmail.com>
2017-05-09 11:48:35 +0800

    f

--- bin/install.js
+++ bin/install.js
@@ -44,7 +44,6 @@ const argv = parseArgs(orignalArgv, {
     'engine-strict',
     'flatten',
     'registry-only',
-    // Use disk cache even on production mode
     'cache-strict',
   ],
   alias: {
@@ -100,6 +99,7 @@ Options:
   --engine-strict: refuse to install (or even consider installing) any package that claims to not be compatible with the current Node.js version.
   --flatten: flatten dependencies by matching ancestors' dependencies
   --registry-only: make sure all packages install from registry. Any package is installed from remote(e.g.: git, remote url) cause install fail.
+  --cache-strict: use disk cache even on production env.
 `
   );
   process.exit(0);
