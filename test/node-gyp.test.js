2313e9fa0c2a1342e2a56f91e1cc2d3bcfc8e00b
fengmk2 <fengmk2@gmail.com>
2017-06-05 02:29:04 +0800

    test: ignore node-gyp.test.js on win32

--- test/node-gyp.test.js
+++ test/node-gyp.test.js
@@ -8,33 +8,35 @@ const mkdirp = require('mkdirp');
 const mm = require('mm');
 const npminstall = require('./npminstall');
 
-describe('test/node-gyp.test.js', function() {
-  const tmp = path.join(__dirname, 'fixtures', 'tmp');
+if (process.platform !== 'win32') {
+  describe('test/node-gyp.test.js', () => {
+    const tmp = path.join(__dirname, 'fixtures', 'tmp');
 
-  function cleanup() {
-    rimraf.sync(tmp);
-  }
-
-  beforeEach(() => {
-    cleanup();
-    mkdirp.sync(tmp);
-  });
-  afterEach(cleanup);
-  afterEach(mm.restore);
-
-  it('should node-gyp work fine', function* () {
-    // remove npm's node-gyp path
-    mm(process.env, 'PATH', process.env.PATH.replace('node-gyp-bin', ''));
-    try {
-      yield npminstall({
-        root: tmp,
-        pkgs: [
-          { name: 'node-icu-charset-detector', version: '0.1.4' },
-        ],
-      });
-    } catch (err) {
-      // ignore
+    function cleanup() {
+      rimraf.sync(tmp);
     }
-    assert(fs.existsSync(path.join(tmp, 'node_modules/node-icu-charset-detector/build')));
+
+    beforeEach(() => {
+      cleanup();
+      mkdirp.sync(tmp);
+    });
+    afterEach(cleanup);
+    afterEach(mm.restore);
+
+    it('should node-gyp work fine', function* () {
+      // remove npm's node-gyp path
+      mm(process.env, 'PATH', process.env.PATH.replace('node-gyp-bin', ''));
+      try {
+        yield npminstall({
+          root: tmp,
+          pkgs: [
+            { name: 'node-icu-charset-detector', version: '0.2.0' },
+          ],
+        });
+      } catch (err) {
+        // ignore
+      }
+      assert(fs.existsSync(path.join(tmp, 'node_modules/node-icu-charset-detector/build')));
+    });
   });
-});
+}
