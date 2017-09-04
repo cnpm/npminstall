0916e696ee4f3dc71ddeb7dfd202077d79cc6c15
dead-horse <dead_horse@qq.com>
2017-09-05 00:54:24 +0800

    f

--- test/fmode.test.js
+++ test/fmode.test.js
@@ -7,27 +7,29 @@ const rimraf = require('rimraf');
 const coffee = require('coffee');
 const npminstall = path.join(__dirname, '../bin/install.js');
 
-describe('test/fmode.test.js', () => {
-  const homedir = path.join(__dirname, 'fixtures', '.tmp');
+if (process.platform !== 'win32') {
+  describe('test/fmode.test.js', () => {
+    const homedir = path.join(__dirname, 'fixtures', '.tmp');
 
-  function cleanup() {
-    rimraf.sync(homedir);
-  }
+    function cleanup() {
+      rimraf.sync(homedir);
+    }
 
-  beforeEach(() => {
-    cleanup();
-    fs.mkdir(homedir);
+    beforeEach(() => {
+      cleanup();
+      fs.mkdir(homedir);
+    });
+    afterEach(cleanup);
+
+    it('should fix file mode success', function* () {
+      yield coffee.fork(npminstall, [ 'array-unique@0.2.1' ], {
+        cwd: homedir,
+      })
+      .debug()
+      .end();
+      const stat = fs.statSync(path.join(homedir, 'node_modules/array-unique/index.js'));
+      // 700 -> 744
+      assert(stat.mode === 33252);
+    });
   });
-  afterEach(cleanup);
-
-  it('should fix file mode success', function* () {
-    yield coffee.fork(npminstall, [ 'array-unique@0.2.1' ], {
-      cwd: homedir,
-    })
-    .debug()
-    .end();
-    const stat = fs.statSync(path.join(homedir, 'node_modules/array-unique/index.js'));
-    // 700 -> 744
-    assert(stat.mode === 33252);
-  });
-});
+}
