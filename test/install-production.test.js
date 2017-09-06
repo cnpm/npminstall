975e4db98bd5abe5f5251dd6c4064a41e7176b8a
fengmk2 <fengmk2@gmail.com>
2017-09-06 13:48:40 +0800

    fix: ignore warnning node_modules on [ '.bin', 'node' ] dir
    https://github.com/cnpm/nodeinstall will create these two dirs first

---
+++ test/install-production.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const coffee = require('coffee');
+const rimraf = require('rimraf');
+const npminstall = path.join(__dirname, '..', 'bin', 'install.js');
+
+describe('test/install-production.test.js', () => {
+  function cleanup(cwd) {
+    rimraf.sync(path.join(cwd, 'node_modules', '.npminstall.done'));
+    rimraf.sync(path.join(cwd, 'node_modules', '.package_versions.json'));
+  }
+
+  describe('warn-node-modules-exists', () => {
+    const cwd = path.join(__dirname, 'fixtures', 'warn-node-modules-exists');
+    before(() => cleanup(cwd));
+    after(() => cleanup(cwd));
+
+    it('should warning node_modules exists on production', () => {
+      return coffee.fork(npminstall, [ '--production' ], { cwd })
+        .debug()
+        .expect('code', 0)
+        .expect('stderr', /npminstall WARN node_modules exists/)
+        .expect('stderr', /contains 1 dirs/)
+        .end();
+    });
+  });
+
+  describe('ignore-warn-node-modules-exists', () => {
+    const cwd = path.join(__dirname, 'fixtures', 'ignore-warn-node-modules-exists');
+    before(() => cleanup(cwd));
+    after(() => cleanup(cwd));
+
+    it('should ignore [ .bin, node ] node_modules', () => {
+      return coffee.fork(npminstall, [ '--production' ], { cwd })
+        .debug()
+        .expect('code', 0)
+        .notExpect('stderr', /npminstall WARN node_modules exists/)
+        .end();
+    });
+  });
+});
