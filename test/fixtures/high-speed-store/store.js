5406c8e60fd4e824e91d225b063bc00259226b76
fengmk2 <fengmk2@gmail.com>
2018-07-19 13:12:17 +0800

    f

--- test/fixtures/high-speed-store/store.js
+++ test/fixtures/high-speed-store/store.js
@@ -1,21 +1,22 @@
 'use strict';
 
+const co = require('co');
 const path = require('path');
 const fs = require('mz/fs');
 const mkdirp = require('mz-modules/mkdirp');
 const utility = require('utility');
 const urllib = require('urllib');
 
-exports.getStream = async (url) => {
+exports.getStream = function* (url) {
   const dir = path.join(__dirname, 'tmp');
-  await mkdirp(dir);
+  yield mkdirp(dir);
   const file = path.join(dir, utility.md5(url) + path.extname(url));
-  if (await fs.exists(file)) {
+  if (yield fs.exists(file)) {
     return fs.createReadStream(file);
   }
 
   const writeStream = fs.createWriteStream(file);
-  await urllib.request(url, {
+  yield urllib.request(url, {
     writeStream,
     timeout: 10000,
     followRedirect: true,
