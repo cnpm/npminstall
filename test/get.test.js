4464362c7eef31d75a665b90326cef494d47e01e
hyj1991 <yeekwanvong@gmail.com>
2019-04-28 13:01:35 +0800

    feat: support authorization (#259)

--- test/get.test.js
+++ test/get.test.js
@@ -1,7 +1,20 @@
 'use strict';
 
+const fs = require('fs');
 const assert = require('assert');
+const mm = require('mm');
+const path = require('path');
+const mockCnpmrc = path.join(__dirname, './fixtures/auth/');
+if (!fs.existsSync(mockCnpmrc + '.cnpmrc')) {
+  fs.writeFileSync(mockCnpmrc + '.cnpmrc', 'registry=https://registry-mock.org/\n//registry-mock.org/:always-auth=true\n//registry-mock.org/:_password="bW9jaw=="\n//registry-mock.org/:username=hyj19911120');
+}
+mm(process.env, 'HOME', mockCnpmrc);
+mm(process.env, 'USERPROFILE', mockCnpmrc);
+// clean cnpm_config.js cache
+delete require.cache[require.resolve('../lib/get')];
+delete require.cache[require.resolve('../lib/cnpm_config')];
 const get = require('../lib/get');
+mm.restore();
 
 describe('test/get.test.js', () => {
   it('should retry on JSON parse error', function* () {
@@ -15,6 +28,25 @@ describe('test/get.test.js', () => {
       assert(false, 'should not run this');
     } catch (err) {
       assert(err.name === 'JSONResponseFormatError');
+      assert(err.res.requestUrls.length === 5);
+    }
+  });
+
+  it('should set auth info into header', function* () {
+    const logger = {
+      warn(msg) {
+        assert(msg.indexOf('[npminstall:get] retry GET') === 0);
+      },
+    };
+    const options = { dataType: 'json' };
+    assert(fs.existsSync(mockCnpmrc + '.cnpmrc'));
+    try {
+      yield get('https://registry-mock.org/mock', options, { console: logger });
+      assert(false, 'should not run this');
+    } catch (err) {
+      const headers = options.headers;
+      assert(headers.Authorization);
+      assert(err.name === 'RequestError');
       assert(err.res.requestUrls.length === 5);
     }
   });
