3754ef6b5d8ce23bd704013664c01b19f3200809
M0D27 <i@0u0b.com>
2019-05-08 17:44:02 +0800

    fix: cnpm install error without user settings (#301)

---
+++ test/install-without-userconfig.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+const rimraf = require('rimraf');
+const coffee = require('coffee');
+const npminstall = path.join(__dirname, '..', 'bin', 'install.js');
+
+const cwd = path.join(__dirname, './fixtures/initial-cnpmrc/');
+
+describe('test/install-without-userconfig.test.js', () => {
+  it('should run cnpm install successfully without cnpmrc userconfig', function* () {
+    function cleanup() {
+      rimraf.sync(path.join(cwd, 'node_modules'));
+    }
+
+    beforeEach(cleanup);
+    afterEach(cleanup);
+
+    yield coffee.fork(npminstall, ['webpack-parallel-uglify-plugin@1.0.0'], { 
+        cwd: cwd, 
+        env: Object.assign({}, process.env, { 
+          USERPROFILE: cwd, 
+          HOME: cwd 
+        }) 
+      })
+      .debug()
+      .expect('code', 0)
+      .end();
+  });
+});
