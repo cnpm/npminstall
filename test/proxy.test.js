0b14789277a58e084458efde0ed6b7adb7a5c13b
fengmk2 <fengmk2@gmail.com>
2019-11-28 19:28:29 +0800

    test: add node 13
    fix proxy test on node < 12

--- test/proxy.test.js
+++ test/proxy.test.js
@@ -2,35 +2,39 @@
 
 const mm = require('mm');
 const coffee = require('coffee');
+const semver = require('semver');
 const proxy = require('./fixtures/reverse-proxy');
 const helper = require('./helper');
 
-describe('test/proxy.test.js', () => {
-  let port;
-  let proxyUrl;
-  before(done => {
-    proxy.listen(0, () => {
-      port = proxy.address().port;
-      proxyUrl = 'http://127.0.0.1:' + port;
-      console.log('proxy: %s', proxyUrl);
-      done();
+// proxy tests only work on node < 12
+if (semver.satisfies(process.version, '< 12.0.0')) {
+  describe('test/proxy.test.js', () => {
+    let port;
+    let proxyUrl;
+    before(done => {
+      proxy.listen(0, () => {
+        port = proxy.address().port;
+        proxyUrl = 'http://127.0.0.1:' + port;
+        console.log('proxy: %s', proxyUrl);
+        done();
+      });
+    });
+    after(() => proxy.close());
+
+    const [ cwd, cleanup ] = helper.tmp();
+
+    beforeEach(cleanup);
+    afterEach(cleanup);
+    afterEach(mm.restore);
+
+    it('should install from proxy', () => {
+      return coffee.fork(helper.npminstall, [
+        '--proxy', proxyUrl,
+        'koa', 'pedding',
+      ], { cwd })
+        .debug()
+        .expect('code', 0)
+        .end();
     });
   });
-  after(() => proxy.close());
-
-  const [ cwd, cleanup ] = helper.tmp();
-
-  beforeEach(cleanup);
-  afterEach(cleanup);
-  afterEach(mm.restore);
-
-  it('should install from proxy', () => {
-    return coffee.fork(helper.npminstall, [
-      '--proxy', proxyUrl,
-      'koa', 'pedding',
-    ], { cwd })
-      .debug()
-      .expect('code', 0)
-      .end();
-  });
-});
+}
