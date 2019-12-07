82a22c1e59b098daa8240a48bee9161e43a5c4d5
fengmk2 <fengmk2@gmail.com>
2019-12-07 23:25:36 +0800

    test: support node 4

--- test/proxy.test.js
+++ test/proxy.test.js
@@ -5,45 +5,48 @@ const rimraf = require('rimraf');
 const mkdirp = require('mkdirp');
 const mm = require('mm');
 const coffee = require('coffee');
-const proxy = require('./fixtures/reverse-proxy');
+const semver = require('semver');
 
-const npminstallBin = path.join(__dirname, '../bin/install.js');
+if (semver.satisfies(process.version, '>= 6.0.0')) {
+  const proxy = require('./fixtures/reverse-proxy');
+  const npminstallBin = path.join(__dirname, '../bin/install.js');
 
-describe('test/proxy.test.js', () => {
-  let port;
-  let proxyUrl;
-  before(done => {
-    proxy.listen(0, () => {
-      port = proxy.address().port;
-      proxyUrl = 'http://127.0.0.1:' + port;
-      console.log('proxy: %s', proxyUrl);
-      done();
+  describe('test/proxy.test.js', () => {
+    let port;
+    let proxyUrl;
+    before(done => {
+      proxy.listen(0, () => {
+        port = proxy.address().port;
+        proxyUrl = 'http://127.0.0.1:' + port;
+        console.log('proxy: %s', proxyUrl);
+        done();
+      });
+    });
+    after(() => proxy.close());
+
+    const tmp = path.join(__dirname, 'fixtures', 'tmp');
+
+    function cleanup() {
+      rimraf.sync(tmp);
+    }
+
+    beforeEach(() => {
+      cleanup();
+      mkdirp.sync(tmp);
+    });
+    afterEach(cleanup);
+    afterEach(mm.restore);
+
+    it('should install from proxy', () => {
+      return coffee.fork(npminstallBin, [
+        '--proxy', proxyUrl,
+        'koa', 'pedding',
+      ], {
+        cwd: tmp,
+      })
+        .debug()
+        .expect('code', 0)
+        .end();
     });
   });
-  after(() => proxy.close());
-
-  const tmp = path.join(__dirname, 'fixtures', 'tmp');
-
-  function cleanup() {
-    rimraf.sync(tmp);
-  }
-
-  beforeEach(() => {
-    cleanup();
-    mkdirp.sync(tmp);
-  });
-  afterEach(cleanup);
-  afterEach(mm.restore);
-
-  it('should install from proxy', () => {
-    return coffee.fork(npminstallBin, [
-      '--proxy', proxyUrl,
-      'koa', 'pedding',
-    ], {
-      cwd: tmp,
-    })
-      .debug()
-      .expect('code', 0)
-      .end();
-  });
-});
+}
