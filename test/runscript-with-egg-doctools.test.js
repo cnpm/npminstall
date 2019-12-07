7373eeff350e129446e7f54968e5e2aed75b1602
fengmk2 <fengmk2@gmail.com>
2019-12-07 22:17:35 +0800

    fix: dont add quotes on node global bin path on Windows
    need to wait for https://github.com/npm/cmd-shim/pull/42
    to be merged and publish a new version.
    
    closes https://github.com/cnpm/npminstall/issues/316

---
+++ test/runscript-with-egg-doctools.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const assert = require('assert');
+const path = require('path');
+const runScript = require('runscript');
+const readJSON = require('../lib/utils').readJSON;
+const npminstall = require('./npminstall');
+const helper = require('./helper');
+
+describe('test/runscript-with-egg-doctools.test.js', () => {
+  const root = helper.fixtures('runscript-with-egg-doctools');
+  const cleanup = helper.cleanup(root);
+
+  beforeEach(cleanup);
+  afterEach(cleanup);
+
+  it('should runscript with egg-doctools bin', async () => {
+    await npminstall({
+      root,
+    });
+    const pkg = await readJSON(path.join(root, 'node_modules', 'egg-doctools', 'package.json'));
+    assert(pkg.name === 'egg-doctools');
+
+    const bin = path.join(root, 'node_modules', '.bin', 'doctools');
+    const stdio = await runScript(`${bin} -V`, { stdio: 'pipe' });
+    assert(stdio.stdout.toString().trim() === '2.9.0');
+  });
+});
