14fac3ba624b649e330b06f8e9547697b423db00
fengmk2 <fengmk2@gmail.com>
2019-12-07 16:51:03 +0800

    f

--- test/uninstallGlobal.test.js
+++ test/uninstallGlobal.test.js
@@ -20,16 +20,28 @@ describe('test/uninstallGlobal.test.js', () => {
       .expect('code', 0)
       .end();
 
-    await coffee.fork(require.resolve('../bin/uninstall'), [
-      `--prefix=${tmp}`,
-      '-g',
-      'mocha',
-    ])
-      .debug()
-      .expect('stdout', /- mocha@\d+\.\d+\.\d+ \.[\/\\]test[\/\\]fixtures[\/\\]tmp[\/\\]lib[\/\\]node_modules[\/\\]mocha/)
-      .expect('stdout', /- mocha@\d+\.\d+\.\d+ \.[\/\\]test[\/\\]fixtures[\/\\]tmp[\/\\]bin[\/\\]mocha/)
-      .expect('stdout', /- mocha@\d+\.\d+\.\d+ \.[\/\\]test[\/\\]fixtures[\/\\]tmp[\/\\]bin[\/\\]_mocha/)
-      .expect('code', 0)
-      .end();
+    if (process.platform === 'win32') {
+      await coffee.fork(require.resolve('../bin/uninstall'), [
+        `--prefix=${tmp}`,
+        '-g',
+        'mocha',
+      ])
+        .debug()
+        .expect('stdout', /- mocha@\d+\.\d+\.\d+/)
+        .expect('code', 0)
+        .end();
+    } else {
+      await coffee.fork(require.resolve('../bin/uninstall'), [
+        `--prefix=${tmp}`,
+        '-g',
+        'mocha',
+      ])
+        .debug()
+        .expect('stdout', /- mocha@\d+\.\d+\.\d+ \.[\/\\]test[\/\\]fixtures[\/\\]tmp[\/\\]lib[\/\\]node_modules[\/\\]mocha/)
+        .expect('stdout', /- mocha@\d+\.\d+\.\d+ \.[\/\\]test[\/\\]fixtures[\/\\]tmp[\/\\]bin[\/\\]mocha/)
+        .expect('stdout', /- mocha@\d+\.\d+\.\d+ \.[\/\\]test[\/\\]fixtures[\/\\]tmp[\/\\]bin[\/\\]_mocha/)
+        .expect('code', 0)
+        .end();
+    }
   });
 });
