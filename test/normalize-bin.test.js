2054f10c442f0dcd3b06e03845413af5af9f66ac
fengmk2 <fengmk2@gmail.com>
2019-12-15 11:38:34 +0800

    fix: the vulnerabilities same as npm cli (#319)
    https://blog.npmjs.org/post/189618601100/binary-planting-with-the-npm-cli

---
+++ test/normalize-bin.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const assert = require('assert');
+const normalize = require('npm-normalize-package-bin');
+
+describe('test/normalize-bin.test.js', () => {
+  it('should fix security bugs', () => {
+    assert.deepEqual(normalize({
+      name: 'foo',
+      bin: '/etc/passwd',
+    }), {
+      name: 'foo',
+      bin: {
+        foo: 'etc/passwd',
+      },
+    });
+
+    assert.deepEqual(normalize({
+      name: 'foo',
+      bin: '../../../../.././etc/passwd',
+    }), {
+      name: 'foo',
+      bin: {
+        foo: 'etc/passwd',
+      },
+    });
+  });
+});
