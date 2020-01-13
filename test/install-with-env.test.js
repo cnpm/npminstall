b1a094ed3945e94759e5125e35a697c7796b9908
fengmk2 <fengmk2@gmail.com>
2020-01-13 15:26:47 +0800

    f

--- test/install-with-env.test.js
+++ test/install-with-env.test.js
@@ -3,15 +3,15 @@
 const coffee = require('coffee');
 const helper = require('./helper');
 
-describe('test/install-with-env.test.js', () => {
+describe('test/install-with-env.test.js', function() {
   const cwd = helper.fixtures('install-husky-4');
   const cleanup = helper.cleanup(cwd);
 
   beforeEach(cleanup);
   afterEach(cleanup);
 
-  it('should install husky@4.0.3', async () => {
-    await coffee.fork(helper.npminstall, [], { cwd })
+  it('should install husky@4.0.3', function* () {
+    yield coffee.fork(helper.npminstall, [], { cwd })
       .debug()
       .expect('code', 0)
       .expect('stdout', /INIT_CWD:.+install-husky-4/)
