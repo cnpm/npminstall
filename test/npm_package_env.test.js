d9f7e4585122684613aadd06cdfcc160e72725c4
fengmk2 <fengmk2@gmail.com>
2020-01-14 14:53:00 +0800

    feat: support npm_config_user_agent env (#323)

--- test/npm_package_env.test.js
+++ test/npm_package_env.test.js
@@ -3,8 +3,10 @@
 const assert = require('assert');
 const rimraf = require('rimraf');
 const path = require('path');
+const coffee = require('coffee');
+const pkg = require('../package.json');
+const helper = require('./helper');
 const readJSON = require('../lib/utils').readJSON;
-const npminstall = require('..');
 
 describe('test/npm_package_env.test.js', () => {
   const root = path.join(__dirname, 'fixtures', 'npm_package_env');
@@ -18,9 +20,12 @@ describe('test/npm_package_env.test.js', () => {
   afterEach(cleanup);
 
   it('should set npm_package_* env on preinstall, postinstall', function* () {
-    yield npminstall({
-      root,
-    });
+    yield coffee.fork(helper.npminstall, [], {
+      cwd: root,
+    })
+      .debug()
+      .expect('code', 0)
+      .end();
     const preinstallEnv = yield readJSON(path.join(root, '.tmp_preinstall'));
     assert(preinstallEnv.npm_package_engines_node === '~0.10.0 || ~0.12.0 || ^4.2.0');
     assert(preinstallEnv.npm_package_engines_foo_bar_0 === '1111');
@@ -31,5 +36,6 @@ describe('test/npm_package_env.test.js', () => {
     assert(postinstallEnv.npm_package_engines_foo_bar_0 === '1111');
     assert(postinstallEnv.npm_package_greenkeeper_ignore_0 === 'glob');
     assert(postinstallEnv.npm_package_greenkeeper_ignore_4 === 'showdown-ghost');
+    assert(postinstallEnv.npm_config_user_agent === `npminstall/${pkg.version} npm/? node/${process.version} ${process.platform} ${process.arch}`);
   });
 });
