ae2d1b9586fbcc5f21b41397a936cbc4cac964b0
DiamondYuan <admin@diamondyuan.com>
2020-06-18 16:56:09 +0800

    teat: add test for install gloabl with alias

--- test/alias.test.js
+++ test/alias.test.js
@@ -5,6 +5,7 @@ const path = require('path');
 const coffee = require('coffee');
 const helper = require('./helper');
 const assert = require('assert');
+const fs = require('mz/fs');
 
 
 let root;
@@ -29,6 +30,29 @@ describe('test/alias.test.js', () => {
         .end();
       const pkg = await helper.readJSON(path.join(root, 'node_modules/egg-alias/package.json'));
       assert(pkg.name === 'egg');
+    });
+  });
+
+  describe('npminstall install -g support alias', () => {
+    const [ tmp, cleanup ] = helper.tmp();
+    let libDir = path.join(tmp, 'lib');
+    if (process.platform === 'win32') {
+      libDir = tmp;
+    }
+    beforeEach(cleanup);
+    afterEach(cleanup);
+    it('should global install work', async () => {
+      await coffee.fork(helper.npminstall, [
+        `--prefix=${tmp}`,
+        '-g',
+        'egg-alias@npm:egg@1',
+      ])
+        .debug()
+        .expect('stdout', /All packages installed/)
+        .expect('code', 0)
+        .end();
+
+      assert(await fs.exists(path.join(libDir, 'node_modules/egg-alias')));
     });
   });
 });
