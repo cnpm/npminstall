5b645c72c8b2df7b37c48a8ffafa724c6c0a8221
gemwuu <gemwuu@163.com>
2021-07-08 18:49:18 +0800

    fix: peerDependencies install failure should show a warning message not throwing error

--- test/peer.test.js
+++ test/peer.test.js
@@ -5,6 +5,7 @@ const path = require('path');
 const assert = require('assert');
 const fs = require('mz/fs');
 const helper = require('./helper');
+const semver = require('semver');
 
 describe('test/peer.test.js', () => {
   describe('unmet root and link', () => {
@@ -41,6 +42,33 @@ describe('test/peer.test.js', () => {
       pkg = await getPkg('node_modules/react-countup/package.json');
       assert(pkg.dependencies.react === '^15.3.2');
       assert(pkg.peerDependencies.react === '>=0.14.0');
+    });
+  });
+
+  describe('skip peer dependencies installation', () => {
+    const tmp = helper.fixtures('skip-peer-dependencies-installation');
+    const cleanup = helper.cleanup(tmp);
+
+    beforeEach(cleanup);
+    afterEach(cleanup);
+    it('should skip peer dependency installation', async () => {
+      await coffee.fork(helper.npminstall, { cwd: tmp })
+        .debug()
+        .expect('code', 0)
+        .expect('stderr', /\[npminstall\] install dependency react@>=\^16.0.0 error: {2}Invalid tag name ">=\^16.0.0": Tags may not have any characters that encodeURIComponent encodes./)
+        .expect('stderr', /\[npminstall\] install dependency react-dom@>=\^16.0.0 error: {2}Invalid tag name ">=\^16.0.0": Tags may not have any characters that encodeURIComponent encodes./)
+
+        .end();
+
+      const pkgReact = await helper.readJSON(path.join(tmp, 'node_modules/react/package.json'));
+      const pkgReactDom = await helper.readJSON(path.join(tmp, 'node_modules/react-dom/package.json'));
+      const pkgReactHovertable = await helper.readJSON(path.join(tmp, 'node_modules/react-hovertable/package.json'));
+      assert.strictEqual(pkgReact.name, 'react');
+      assert.strictEqual(semver(pkgReact.version).major, 17);
+      assert.strictEqual(pkgReactDom.name, 'react-dom');
+      assert.strictEqual(semver(pkgReactDom.version).major, 17);
+      assert.strictEqual(pkgReactHovertable.name, 'react-hovertable');
+      assert.strictEqual(pkgReactHovertable.version, '0.3.0');
     });
   });
 
