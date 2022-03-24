c12a5120b72d2db8721b3a686a07d88cace800bb
fengmk2 <fengmk2@gmail.com>
2022-03-24 17:17:41 +0800

    f

--- test/install-with-python3.test.js
+++ test/install-with-python3.test.js
@@ -3,18 +3,20 @@
 const coffee = require('coffee');
 const helper = require('./helper');
 
-describe('test/install-with-python3.test.js', () => {
-  const cwd = helper.fixtures('install-raw-socket');
-  const cleanup = helper.cleanup(cwd);
+if (process.platform !== 'win32') {
+  describe('test/install-with-python3.test.js', () => {
+    const cwd = helper.fixtures('install-raw-socket');
+    const cleanup = helper.cleanup(cwd);
 
-  beforeEach(cleanup);
-  afterEach(cleanup);
+    beforeEach(cleanup);
+    afterEach(cleanup);
 
-  it('should install raw-socket with python3', async () => {
-    // https://github.com/cnpm/npminstall/issues/384
-    await coffee.fork(helper.npminstall, [ '-d' ], { cwd })
-      .debug()
-      .expect('code', 0)
-      .end();
+    it('should install raw-socket with python3', async () => {
+      // https://github.com/cnpm/npminstall/issues/384
+      await coffee.fork(helper.npminstall, [ '-d' ], { cwd })
+        .debug()
+        .expect('code', 0)
+        .end();
+    });
   });
-});
+}
