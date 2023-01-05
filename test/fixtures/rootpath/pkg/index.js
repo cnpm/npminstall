6e88c5f5e836fa9bd8af6702867b842a5086fef0
fengmk2 <fengmk2@gmail.com>
2023-01-05 22:14:12 +0800

    fix: auto set INIT_CWD env on install scripts
    https://github.com/vercel/turbo/issues/485

--- test/fixtures/rootpath/pkg/index.js
+++ test/fixtures/rootpath/pkg/index.js
@@ -1,5 +1,7 @@
-const path = require('path');
-
 console.log('process.env.npm_rootpath: %j', process.env.npm_rootpath);
+console.log('process.env.INIT_CWD: %j', process.env.INIT_CWD);
+console.log('process.env.npm_rootpath %s', process.env.npm_rootpath ? 'exists' : 'not exists');
+console.log('process.env.INIT_CWD %s', process.env.INIT_CWD ? 'exists' : 'not exists');
 console.log('__dirname: %j', __dirname);
-console.log('hello process.env.npm_rootpath is %s', process.env.npm_rootpath === path.dirname(path.dirname(__dirname)));
+console.log('process.cwd(): %j', process.cwd());
+
