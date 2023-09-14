adf537d58e8c024c6d47603ffc524abc3e685d91
tianding.wk <tianding.wk@alipay.com>
2023-09-14 17:16:01 +0800

    feat: add npm package-lock.json support

---
+++ test/install-with-lockfile.test.js
@@ -0,0 +1,45 @@
+const coffee = require('coffee');
+const path = require('node:path');
+const assert = require('node:assert');
+const fs = require('node:fs/promises');
+const { lockfileConverter } = require('../lib/lockfile_resolver');
+const Nested = require('../lib/nested');
+const helper = require('./helper');
+
+describe('test/install-with-lockfile.test.js', () => {
+  const cwd = helper.fixtures('lockfile');
+  const lockfile = require(path.join(cwd, 'package-lock.json'));
+  const nested = new Nested([]);
+  const cleanup = helper.cleanup(cwd);
+
+  beforeEach(cleanup);
+  afterEach(cleanup);
+
+  it('should install successfully', async () => {
+    await coffee.fork(
+      helper.npminstall,
+      [
+        '--lockfile-path',
+        path.join(cwd, 'package-lock.json'),
+      ], { cwd })
+      .debug()
+      .expect('code', 0)
+      .end();
+    assert.strictEqual(
+      await fs.readlink(path.join(cwd, 'node_modules', 'lodash.has3'), 'utf8'),
+      '.store/lodash.has@3.2.1/node_modules/lodash.has'
+    );
+    assert.strictEqual(
+      await fs.readlink(path.join(cwd, 'node_modules', 'lodash.has'), 'utf8'),
+      '.store/lodash.has@4.0.0/node_modules/lodash.has'
+    );
+  });
+
+  it('should convert package-lock.json to .dependencies-tree.json successfully', () => {
+    const dependenciesTree = lockfileConverter(lockfile, {
+      ignoreOptionalDependencies: true,
+    }, nested);
+
+    assert.strictEqual(Object.keys(dependenciesTree).length, 12);
+  });
+});
