fecd257a5baa07c2984c9f72087cd3569f495716
elrrrrrrr <elrrrrrrr@gmail.com>
2023-09-15 17:44:22 +0800

    feat: add npm package-lock.json support for 6.x (#463)
    > pick #462
    
    ---------
    
    Co-authored-by: tianding.wk <tianding.wk@alipay.com>

---
+++ test/install-with-lockfile.test.js
@@ -0,0 +1,49 @@
+const coffee = require('coffee');
+const path = require('node:path');
+const assert = require('node:assert');
+const fs = require('node:fs/promises');
+const { lockfileConverter } = require('../lib/lockfile_resolver');
+const Nested = require('../lib/nested');
+const helper = require('./helper');
+
+describe('test/install-with-lockfile.test.js', () => {
+  const cwd = helper.fixtures('lockfile');
+  const lockfile = require(path.join(cwd, 'package-lock.json'));
+  const nested = new Nested([]);
+  const cleanup = helper.cleanup(cwd);
+
+  beforeEach(cleanup);
+  afterEach(cleanup);
+
+  // the Windows path sucks, shamefully skip these tests
+  if (process.platform !== 'win32') {
+    it('should install successfully', async () => {
+      await coffee.fork(
+        helper.npminstall,
+        [
+          '--lockfile-path',
+          path.join(cwd, 'package-lock.json'),
+        ], { cwd })
+        .debug()
+        .expect('code', 0)
+        .end();
+      assert.strictEqual(
+        await fs.readlink(path.join(cwd, 'node_modules', 'lodash.has3'), 'utf8'),
+        '_lodash.has@3.2.1@lodash.has'
+      );
+
+      assert.strictEqual(
+        await fs.readlink(path.join(cwd, 'node_modules', 'lodash.has'), 'utf8'),
+        '_lodash.has@4.0.0@lodash.has'
+      );
+    });
+
+    it('should convert package-lock.json to .dependencies-tree.json successfully', () => {
+      const dependenciesTree = lockfileConverter(lockfile, {
+        ignoreOptionalDependencies: true,
+      }, nested);
+
+      assert.strictEqual(Object.keys(dependenciesTree).length, 12);
+    });
+  }
+});
